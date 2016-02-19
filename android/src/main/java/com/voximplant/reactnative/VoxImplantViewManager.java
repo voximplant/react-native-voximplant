dce9da77f5f155f4e61be0a94426e3f741d0f654
Alexey Aylarov <alexey@voximplant.com>
2016-02-19 16:29:47 +0300

    Upgrade to React Native 0.20

--- android/src/main/java/com/voximplant/reactnative/VoxImplantViewManager.java
+++ android/src/main/java/com/voximplant/reactnative/VoxImplantViewManager.java
@@ -3,7 +3,8 @@ package com.voximplant.reactnative;
 import android.util.Log;
 import javax.annotation.Nullable;
 import android.content.Context;
-import com.facebook.react.uimanager.*;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class VoxImplantViewManager extends SimpleViewManager<VoxImplantRendererView>
