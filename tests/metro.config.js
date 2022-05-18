1cec6b97d44dbfafae2e510120e14d5a974936d5
Ilya Petrosyan <im.clean@yandex.ru>
2022-05-18 12:37:59 +0300

    DRun react-native-voximplant as local dependency, not throught metro

--- tests/metro.config.js
+++ tests/metro.config.js
@@ -1,26 +1,4 @@
-const { resolve, join } = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
-
-const config = {
-  resolver: {
-    blocklist: exclusionList([
-      new RegExp(`^${escape(resolve(__dirname, '..', 'node_modules'))}\\/.*$`),
-    ]),
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => {
-          if (typeof name !== 'string') {
-            return target[name];
-          }
-          if (name && name.startsWith && name.startsWith('react-native-voximplant')) {
-            return join(__dirname, `../`);
-          }
-          return join(__dirname, `node_modules/${name}`);
-        },
-      },
-    ),
-  },
+module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -29,7 +7,4 @@ const config = {
       },
     }),
   },
-  watchFolders: [resolve(__dirname, '../')],
 };
-
-module.exports = config;
